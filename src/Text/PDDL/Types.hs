6be7650b064eae7a67cf0f18e96de1e3108d3dc6
Trevor Elliott <trevor@galois.com>
2014-11-23 12:27:39 -0800

    Initial import

---
+++ src/Text/PDDL/Types.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module Text.PDDL.Types where
+
+import qualified Data.Text as S
+
+
+data PDDL = PDDLProblem Problem
+          | PDDLDomain Domain
+            deriving (Show)
+
+type Name = S.Text
+type Pred = Name
+
+type Var  = S.Text
+
+data Domain = Domain { dName     :: Name
+                     , dRequires :: [Requirement]
+                     , dTypes    :: TypedList Name
+                     , dConsts   :: TypedList Name
+                     , dPredSigs :: [PredSig]
+                     , dFuns     :: TypedList Function
+                     , dCons     :: [Con]
+                     , dActions  :: [Action]
+                     } deriving (Show)
+
+data Problem = Problem { pName     :: !Name
+                       , pDomain   :: !Name
+                       , pRequires :: [Requirement]
+                       } deriving (Show)
+
+data Requirement = ReqStrips
+                 | ReqTyping
+                 | ReqNegativePreconditions
+                 | ReqDisjunctivePreconditions
+                 | ReqEquality
+                 | ReqExistentialPreconditions
+                 | ReqUniversalPreconditions
+                 | ReqQuantifiedPreconditions
+                 | ReqConditionalEffects
+                 | ReqFluents
+                 | ReqNumericFluents
+                 | ReqObjectFluents
+                 | ReqAdl
+                 | ReqDurativeActions
+                 | ReqDurationInequalities
+                 | ReqContinuousEffects
+                 | ReqDerivedPredicates
+                 | ReqTimedInitialLiterals
+                 | ReqPreferences
+                 | ReqConstraints
+                 | ReqActionCosts
+                   deriving (Show)
+
+data TypedList a = UntypedList [a]
+                 | TypedList [Typed a]
+                   deriving (Show,Functor)
+
+data Typed a = Typed { tType :: Type
+                     , tVal  :: a
+                     } deriving (Show,Functor)
+
+data Type = Type Name
+          | Either [Name]
+            deriving (Show)
+
+data PredSig = PredSig { psName :: Name
+                       , psArgs :: TypedList Name
+                       } deriving (Show)
+
+data Function = Function { funName :: Name
+                         , funArgs :: TypedList Name
+                         } deriving (Show)
+
+data Term = TVar Var
+          | TName Name
+            deriving (Show)
+
+data GoalDesc = GDAnd [GoalDesc]
+              | GDOr [GoalDesc]
+              | GDNot GoalDesc
+              | GDImply GoalDesc GoalDesc
+              | GDExists (TypedList Var) GoalDesc
+              | GDForall (TypedList Var) GoalDesc
+              | GDLit (Literal Term)
+                deriving (Show)
+
+data Con = CForall (TypedList Name) [Con]
+         | CAtEnd GoalDesc
+         | CAlways GoalDesc
+         | CSometime GoalDesc
+         | CWithin Int GoalDesc
+         | CAtMostOnce GoalDesc
+         | CSometimeAfter GoalDesc GoalDesc
+         | CSometimeBefore GoalDesc GoalDesc
+         | CAlwaysWithin Int GoalDesc GoalDesc
+         | CHoldDuring Int Int GoalDesc
+         | CHoldAfter Int GoalDesc
+           deriving (Show)
+
+data Literal a = Literal Bool (Formula a)
+                 deriving (Show)
+
+data Formula a = FPred Pred [a]
+               | FEq a a
+                 deriving (Show)
+
+data Action = Action
+              deriving (Show)
